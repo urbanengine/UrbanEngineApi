d7beee6b40afe61f0b4c4f5750807f2c86804d70
Tyler Hughes <tyler@tylerhughes.info>
2018-08-08 07:40:33 -0500

    Added Autofac dependency

--- UrbanEngine.Web/Startup.cs
+++ UrbanEngine.Web/Startup.cs
@@ -1,31 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using UrbanEngine.Core.Interfaces;
+using UrbanEngine.Infrastructure.Managers;
+using UrbanEngine.Infrastructure.Repository;
 
 namespace urban_engine_api
 {
     public class Startup
     {
+        #region Properties
+
+        public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Constructor
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        #endregion
+
+        #region Public Methods
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAutofac();
+
+            RegisterControllers(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,5 +54,28 @@ namespace urban_engine_api
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
+        // Use this method to register things directly with Autofac
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            RegisterManagers(builder);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void RegisterControllers(IServiceCollection services)
+        {
+            services.AddTransient<IUserManager, UserManager>();
+        }
+
+        private void RegisterManagers(ContainerBuilder builder)
+        {
+            builder.RegisterType<IUserManager>().As<UserManager>();
+            builder.RegisterType<IDbRepository>().As<DbRepository>();
+        }
+
+        #endregion
     }
 }
