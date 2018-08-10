3061a0c54fc2bfda386b6a89dea1590b694f3a6f
HUGHES Tyler (Tyler) <tyler.hughes@hexagonsi.com>
2018-08-10 16:31:19 -0500

    Added project for unit tests

--- UrbanEngine.Web/Startup.cs
+++ UrbanEngine.Web/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
-namespace urban_engine_api
-{
+namespace urban_engine_api {
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,6 +20,7 @@ namespace urban_engine_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAutofac();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +37,12 @@ namespace urban_engine_api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        // Use this method to register things directly with AutoFac
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            
         }
     }
 }
