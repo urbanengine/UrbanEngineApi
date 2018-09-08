0afd809be440f465c406817857e52a675eb97b98
cslusk <scott.lusk@hexagonsi.com>
2018-09-08 15:59:20 -0400

    added AboutController as a test end point to ensure can call service
    updated launchSettings.json launchUrl to be api/about since api/values does not actually exist

---
+++ UrbanEngine.Web/Controllers/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection; 
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrbanEngine.Web.Controllers {
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class AboutController : ControllerBase {
+         
+        [HttpGet]
+        public IActionResult GetVersion() {
+            return Ok( new {
+                Service = new {
+                    Name = GetShortName(), 
+                    Version = GetAssemblyVersion()
+                }
+            } ); 
+        }
+
+        // TODO: we can move these out into a helper class to be reused across other services 
+
+        private string GetAssemblyVersion() {
+            var assemblyName = Assembly.GetCallingAssembly().GetName(); 
+            var version = assemblyName.Version.ToString(); 
+            return version;
+        }
+
+        private string GetShortName() {
+            var assemblyName = Assembly.GetCallingAssembly().GetName();
+            var name = assemblyName.Name;
+            var lastIdx = name.LastIndexOf( '.' );
+            var shortName = name.Substring( lastIdx + 1 );
+            return shortName;
+        }
+
+    }
+}
