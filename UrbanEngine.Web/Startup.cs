079c5dd94e33ae7f8cfdec225ade5713a0276a03
cslusk <scott.lusk@hexagonsi.com>
2018-09-08 22:02:53 -0400

    added DI for registering the ApplicationDbContext started with, using an in memory only db for right now that has no data
    added DI for IDbRepository and IUserManager

--- UrbanEngine.Web/Startup.cs
+++ UrbanEngine.Web/Startup.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting; 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UrbanEngine.Core.Interfaces;
+using UrbanEngine.Infrastructure.Managers;
+using UrbanEngine.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using UrbanEngine.Infrastructure.Context;
 
 namespace urban_engine_api {
     public class Startup {
@@ -25,6 +30,21 @@ namespace urban_engine_api {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices( IServiceCollection services ) {
             services.AddMvc().SetCompatibilityVersion( CompatibilityVersion.Version_2_1 );
+
+            #region Dependency Injection 
+
+            // context   
+            services.AddDbContext<ApplicationDbContext>( options =>
+                options.UseInMemoryDatabase( "InMemoryDb" ) // TODO, switch over to actual db implementation 
+            );
+
+            // repository  
+            services.AddScoped<IDbRepository, DbRepository>(); 
+
+            // managers 
+            services.AddTransient<IUserManager, UserManager>(); 
+             
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
