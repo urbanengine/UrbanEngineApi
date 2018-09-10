84c3809b8c637d97b5cc87e850b689682c7e324a
scottluskcis <scottlusk.cis@outlook.com>
2018-09-09 20:00:55 -0400

    removed api/ from route as requested

--- UrbanEngine.Web/Controllers/AboutController.cs
+++ UrbanEngine.Web/Controllers/AboutController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace UrbanEngine.Web.Controllers {
-    [Route( "api/[controller]" )]
+    [Route( "[controller]" )]
     [ApiController]
     public class AboutController : ControllerBase {
          
