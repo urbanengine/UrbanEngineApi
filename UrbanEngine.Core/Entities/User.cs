359b74dec7da0ab1db4c3f14895bbc0cbc9c876e
scottluskcis <scottlusk.cis@outlook.com>
2018-09-12 21:31:55 -0400

    added xml documentation to classes to test in generation with swashbuckle

--- UrbanEngine.Core/Entities/User.cs
+++ UrbanEngine.Core/Entities/User.cs
@@ -1,13 +1,27 @@
-﻿using System;
-
-namespace UrbanEngine.Core
-{
-    public class User
-    {
+﻿namespace UrbanEngine.Core {
+    /// <summary>
+    /// a user that interacts with the system
+    /// </summary>
+    public class User {
+        /// <summary>
+        /// uniquely identifies a user
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// first name or given name of the user 
+        /// </summary>
         public string FirstName { get; set; }
+        /// <summary>
+        /// last name or surname of the user 
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// whether or not the user account is considered an Elite account 
+        /// </summary>
         public bool IsElite { get; set; }
+        /// <summary>
+        /// identifier associated with Auth0 account 
+        /// </summary>
         public string AuthZeroId { get; set; }
     }
 }
