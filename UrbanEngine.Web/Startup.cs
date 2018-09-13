99d9218beec69add560ed9c126916c164ffa7fe6
scottluskcis <scottlusk.cis@outlook.com>
2018-09-12 23:02:09 -0400

    wired up Api Versioning via explorer in the Startup to generate docs for each api version

--- UrbanEngine.Web/Startup.cs
+++ UrbanEngine.Web/Startup.cs
@@ -1,21 +1,24 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting; 
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using UrbanEngine.Core.Interfaces;
-using UrbanEngine.Infrastructure.Managers;
-using UrbanEngine.Infrastructure.Repository;
-using Microsoft.EntityFrameworkCore;
-using UrbanEngine.Infrastructure.Context;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Swashbuckle.AspNetCore.Swagger;
-using System.Reflection;
-using System.IO;
-using System;
+﻿namespace urban_engine_api {
+    using System.Reflection;
+    using System.IO;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.PlatformAbstractions;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using Swashbuckle.AspNetCore.Swagger;
 
-namespace urban_engine_api {
+    using UrbanEngine.Core.Interfaces;
+    using UrbanEngine.Web.Configuration;
+    using UrbanEngine.Infrastructure.Managers;
+    using UrbanEngine.Infrastructure.Repository;
+    using UrbanEngine.Infrastructure.Context;
+      
     public class Startup {
         #region Properties
 
@@ -44,35 +47,52 @@ namespace urban_engine_api {
                     options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                 } );
 
-            #region Swagger 
-             
-            // Register the Swagger generator, defining one or more Swagger documents
-            services.AddSwaggerGen( c => {
-                c.SwaggerDoc( "v1", new Info {
-                    Version = "v1",
-                    Title = "UrbanEngine.Web",
-                    Description = "Urban Engine API"
-                    // TODO add all of these details to match Urban Engine information 
-                    //, Contact = new Contact { }
-                    //, License = new License { }
-                    //, TermsOfService = ""
+
+            #region Versioning
+
+            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+            // note: the specified format code will format the version as "'v'major[.minor][-status]"
+            services.AddMvcCore().AddVersionedApiExplorer(
+                options => {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
                 } );
 
-                // Set the comments path for the Swagger JSON and UI.
-                string[] xmlFiles = new string[] {
-                    // include xml for this assmebly 
-                    $"{Assembly.GetEntryAssembly().GetName().Name}.xml",
-                    // include other assemblys
-                    "UrbanEngine.Core.xml" 
-                };
+            services.AddApiVersioning( options => {
+                // reporting api versions will return the headers "api-supported-versions" and "api-deprecated-versions" 
+                options.ReportApiVersions = true;
 
-                foreach( var xmlFile in xmlFiles ) {
-                    var xmlPath = Path.Combine( AppContext.BaseDirectory, xmlFile );
-                    c.IncludeXmlComments( xmlPath );
-                }
+                // alternative, if we would rather include api-version in the Headers of the request, for now use it in URL
+                // options.ApiVersionReader = new HeaderApiVersionReader("api-version"); 
+            } );
 
+            #endregion
+
+            #region Swagger 
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen( options => {  
                 // since JS uses camelcase our docs should as well 
-                c.DescribeAllParametersInCamelCase();
+                options.DescribeAllParametersInCamelCase(); 
+
+                // resolve the IApiVersionDescriptionProvider service
+                // note: that we have to build a temporary service provider here because one has not been created yet
+                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                // add a swagger document for each discovered API version
+                // note: you might choose to skip or document deprecated API versions differently
+                foreach( var description in provider.ApiVersionDescriptions ) {
+                    options.SwaggerDoc( description.GroupName, CreateInfoForApiVersion( description ) );
+                }
+                 
+                // set operation filter to work in conjunction with API Versioning 
+                options.OperationFilter<SwaggerDefaultValues>();
+
+                // integrate xml comments
+                options.IncludeXmlComments( XmlCommentsFilePath );
             } );
 
             #endregion
@@ -94,7 +114,7 @@ namespace urban_engine_api {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure( IApplicationBuilder app, IHostingEnvironment env ) {
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider ) {
             // to disable AppInsights Telemetry uncomment next two lines 
             // var configuration = app.ApplicationServices.GetService<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>();
             // configuration.DisableTelemetry = true;
@@ -111,14 +131,42 @@ namespace urban_engine_api {
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI( c => {
-                c.SwaggerEndpoint( "/swagger/v1/swagger.json", "UrbanEngine.Web V1" );
+            app.UseSwaggerUI( options => {
+                // build a swagger endpoint for each discovered API version
+                foreach( var description in provider.ApiVersionDescriptions ) {
+                    options.SwaggerEndpoint( $"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant() );
+                }
             } );
 
             #endregion
 
             app.UseHttpsRedirection(); 
             app.UseMvc(); 
+        }
+
+        static string XmlCommentsFilePath {
+            get {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof( Startup ).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine( basePath, fileName );
+            }
+        }
+
+        static Info CreateInfoForApiVersion( ApiVersionDescription description ) {
+            var info = new Info() {
+                Title = $"Urban Engine API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "UrbanEngine.Web API",
+                Contact = new Contact() { Name = "Tyler Hughes", Email = "tyler@urbanengine.org" },
+                TermsOfService = "",
+                License = new License() { Name = "Apache 2.0", Url = "https://www.apache.org/licenses/LICENSE-2.0.html" }
+            };
+
+            if( description.IsDeprecated ) {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
         }
 
         #endregion
