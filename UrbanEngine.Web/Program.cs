12cdb0c69c9fe8a863a8055e3ffc34967eebbf22
scottluskcis <scottlusk.cis@outlook.com>
2018-10-01 22:25:24 -0400

    program now calls migrate on db context to create or update the database when program is launched

--- UrbanEngine.Web/Program.cs
+++ UrbanEngine.Web/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UrbanEngine.Infrastructure.Context;
 
 namespace urban_engine_api {
     public class Program {
@@ -7,10 +10,24 @@ namespace urban_engine_api {
             BuildWebHost( args ).Run();
         }
 
-        public static IWebHost BuildWebHost( string[] args ) =>
-            WebHost.CreateDefaultBuilder( args )
+        public static IWebHost BuildWebHost( string[] args ) {
+            var host = WebHost.CreateDefaultBuilder( args )
                 .UseApplicationInsights()
-                .UseStartup<Startup>() 
+                .UseStartup<Startup>()
                 .Build();
+
+            CreateOrMigrateDatabase<UrbanEngineContext>( host ); 
+
+            return host;
+        }
+         
+        // this will create database if not exists or update it to latest if it does 
+        static void CreateOrMigrateDatabase<TContext>( IWebHost host ) where TContext : DbContext {
+            using( var scope = host.Services.CreateScope() )
+            using( var context = scope.ServiceProvider.GetService<TContext>() ) {
+                context.Database.Migrate();
+            }
+        }
+
     }
 }
