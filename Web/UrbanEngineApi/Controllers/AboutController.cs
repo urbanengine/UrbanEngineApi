5ce8ddfe3d416accfad20b6fd0456bc94932ff58
scottluskcis <scottlusk.cis@gmail.com>
2019-03-23 12:07:36 -0400

    add about controller, remove values controller

---
+++ Web/UrbanEngineApi/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrbanEngineApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Name = nameof(UrbanEngineApi)
+            });
+        }
+    }
+}
