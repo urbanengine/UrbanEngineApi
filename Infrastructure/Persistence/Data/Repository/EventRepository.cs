744c83cdbc8f6868a38d1cd09a5c0cd78286d85f
scottluskcis <scottlusk.cis@gmail.com>
2019-03-23 15:18:47 -0400

    added EventRepository stub

---
+++ Infrastructure/Persistence/Data/Repository/EventRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanEngine.Core.Application.Interfaces.Persistence.Data;
+
+namespace UrbanEngine.Infrastructure.Persistence.Data.Repository
+{
+    public class EventRepository : IEventRepository
+    {
+    }
+}
