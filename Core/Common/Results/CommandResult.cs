393ca7320bab83c674957d53253e723a6146970c
scottluskcis <scottlusk.cis@gmail.com>
2019-07-04 16:58:12 -0400

    CommandResultWithData added to support sending data with response

--- Core/Common/Results/CommandResult.cs
+++ Core/Common/Results/CommandResult.cs
@@ -11,4 +11,21 @@
             Success = success;
         }
     }
+
+    public class CommandResultWithData : CommandResult
+    {
+        public object Data { get; private set; }
+
+        public CommandResultWithData(object data, string message, int? statusCode, bool? success)
+            : base(message, statusCode, success)
+        {
+            Data = data;
+        }
+    }
+
+    public class CommandResultWithData<T> : CommandResultWithData
+    {
+        public CommandResultWithData(T data, string message, int? statusCode, bool? success)
+            : base(data, message, statusCode, success) { }
+    }
 }
