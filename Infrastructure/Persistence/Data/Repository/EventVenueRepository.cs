a712955901c107e243b2fca33d3061c7b9633f65
scottluskcis <scottlusk.cis@gmail.com>
2019-07-04 08:36:39 -0400

    add repository for managing event venues

---
+++ Infrastructure/Persistence/Data/Repository/EventVenueRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrbanEngine.Core.Application.Entities.ScheduleAggregate;
+
+namespace UrbanEngine.Infrastructure.Persistence.Data.Repository
+{
+    public class EventVenueRepository : EfRepository<EventVenue>
+    {
+        public EventVenueRepository(DbContext dbContext) : base(dbContext) { }
+    }
+}
