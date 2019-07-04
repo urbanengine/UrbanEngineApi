bb7608861669ea7d496df97403136116a15d6612
scottluskcis <scottlusk.cis@gmail.com>
2019-07-04 16:08:40 -0400

    format properties

--- Services/UrbanEngineApi/V1/Models/Venues/EventVenueListItem.cs
+++ Services/UrbanEngineApi/V1/Models/Venues/EventVenueListItem.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using UrbanEngine.Core.Application.Entities.ScheduleAggregate;
 using UrbanEngine.Core.Application.Venues;
 
@@ -11,7 +13,13 @@ namespace UrbanEngine.Services.UrbanEngineApi.V1.Models.Venues
     /// </summary>
     public class EventVenueListItem : IEventVenueModel
     {
+        public long VenueId { get; set; }
+
         public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Region { get; set; }
 
         [JsonIgnore]
         public Expression<Func<EventVenue, IEventVenueModel>> Projection
@@ -20,10 +28,22 @@ namespace UrbanEngine.Services.UrbanEngineApi.V1.Models.Venues
             {
                 return x => new EventVenueListItem
                 {
-                    Name = x.Name
+                    VenueId = x.Id,
+                    Name = x.Name,
+                    Location = FormatLocation(x.Address, x.Address2, x.City, x.State, x.PostalCode, x.Country),
+                    Region = x.Region.Name
                 };
             }
         }
         
+        private string FormatLocation(string address, string address2, string city, string state, string zip, string country)
+        {
+            var values = new[]
+            {
+                address, address2, city, state, zip, country
+            };
+
+            return string.Join(" ", values.Where(p => !string.IsNullOrEmpty(p)));
+        }
     }
 }
