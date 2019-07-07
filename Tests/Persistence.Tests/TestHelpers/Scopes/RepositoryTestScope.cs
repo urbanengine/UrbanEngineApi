142b2a4190cede60b2b75bca015372ddeac0c2fc
scottluskcis <scottlusk.cis@gmail.com>
2019-07-07 19:01:45 -0400

    added test classes for testing repository and adding in additional test data such as FakeEntity.

---
+++ Tests/Persistence.Tests/TestHelpers/Scopes/RepositoryTestScope.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using UrbanEngine.Core.Application.Interfaces.Persistence.Data;
+
+namespace UrbanEngine.Tests.Persistence.Tests.TestHelpers.Scopes
+{
+    public abstract class RepositoryTestScope<TRepository> : TestScope<TRepository>, IDisposable
+        where TRepository : class, IRepository
+    {
+        private UrbanEngineTestDbContext _context;
+        private SqliteConnection _connection;
+
+        public RepositoryTestScope() : base()
+        {
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+            _connection?.Close();
+        }
+
+        internal virtual UrbanEngineTestDbContext GetDbContext()
+        {
+            if(_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+            }
+
+            if(_context == null)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<UrbanEngineTestDbContext>();
+                optionsBuilder.UseSqlite(_connection);
+
+                _context = new UrbanEngineTestDbContext(optionsBuilder.Options);
+                _context.Database.EnsureCreated();  
+            }
+            return _context;
+        }
+    }
+
+}
---
+++ Tests/Persistence.Tests/TestHelpers/TestSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using UrbanEngine.Core.Application.Specifications;
+
+namespace UrbanEngine.Tests.Persistence.Tests.TestHelpers
+{
+    internal class TestSpecification : BaseSpecification<FakeEntity>
+    {
+        public TestSpecification()
+        {
+        }
+
+        public TestSpecification(Expression<Func<FakeEntity, bool>> criteria)
+            : base(criteria)
+        { 
+        }
+
+        public static TestSpecification AllItems => new TestSpecification(p => true);
+
+        public static TestSpecification NoItems => new TestSpecification(p => false);
+    }
+}
