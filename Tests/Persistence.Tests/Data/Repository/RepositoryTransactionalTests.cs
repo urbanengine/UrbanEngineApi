a5aa7658aea60cf0b4c65a63e6d1b98c23b7ecd2
scottluskcis <scottlusk.cis@gmail.com>
2019-07-07 19:40:54 -0400

    create class for handling transaction based unit tests for CUD operations

---
+++ Tests/Persistence.Tests/Data/Repository/RepositoryTransactionalTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrbanEngine.Infrastructure.Persistence.Data.Repository;
+using UrbanEngine.Tests.Persistence.Tests.TestHelpers;
+using UrbanEngine.Tests.Persistence.Tests.TestHelpers.Scopes;
+
+namespace UrbanEngine.Tests.Persistence.Tests.Data.Repository
+{
+    [TestClass]
+    public class RepositoryTransactionalTests
+    {
+        [TestMethod]
+        public async Task CreateAsync_Should_CreateNewItem()
+        {
+            var newEntity = new FakeEntity(4, "NewItem", "Test");
+            var expectedResult = newEntity.Id;
+
+            using (var scope = new DefaultScope())
+            {
+                var result = await scope.InstanceUnderTest.CreateAsync(newEntity);
+                var actualResult = result?.Id;
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        public async Task UpdateAsync_Should_UpdateExistingItem()
+        {
+            FakeEntity itemToUpdate = null;
+            using (var scope = new DefaultScope())
+            {
+                itemToUpdate = await scope.InstanceUnderTest.GetByIdAsync(1);
+                itemToUpdate.Name = "Updated Name";
+
+                var result = await scope.InstanceUnderTest.UpdateAsync(itemToUpdate);
+                Assert.IsTrue(result == 1);
+            } 
+        }
+
+        private class DefaultScope : RepositoryTestScope<TestTransactionalRepository>
+        {
+            public DefaultScope()
+            {
+                InstanceUnderTest = new TestTransactionalRepository(GetDbContext());
+            }
+        }
+
+        // inherint from EfRepository to test all the inherited functionality
+        private class TestTransactionalRepository : EfRepository<FakeEntity>
+        {
+            public TestTransactionalRepository(UrbanEngineTestDbContext dbContext)
+                : base(dbContext) { }
+        }
+    }
+}
