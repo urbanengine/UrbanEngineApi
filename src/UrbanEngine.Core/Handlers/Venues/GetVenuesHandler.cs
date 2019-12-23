ba12c505d1868ad02f9b4c7a6fdc97ca29789627
scottluskcis <scottlusk.cis@gmail.com>
2019-12-23 13:43:19 -0500

    wire up handler and specification

--- src/UrbanEngine.Core/Handlers/Venues/GetVenuesHandler.cs
+++ src/UrbanEngine.Core/Handlers/Venues/GetVenuesHandler.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UrbanEngine.Core.Entities;
+using UrbanEngine.Core.Managers.Venues;
+using UrbanEngine.Core.Messages.Venues;
+using UrbanEngine.Core.Models.Venues;
+using UrbanEngine.Core.Specifications.Venues;
+using UrbanEngine.SharedKernel.Results;
 
 namespace UrbanEngine.Core.Handlers.Venues
 {
-    public class GetVenuesHandler
+    public class GetVenuesHandler : IRequestHandler<GetEventVenuesMessage, QueryResult<IEnumerable<EventVenueListItemDto>>>
     {
+        private readonly IEventVenueManager _manager;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public GetVenuesHandler(IEventVenueManager manager, IMapper mapper, ILogger<GetVenuesHandler> logger)
+        {
+            _manager = manager;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<QueryResult<IEnumerable<EventVenueListItemDto>>> Handle(GetEventVenuesMessage request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{nameof(GetVenuesHandler)} - Handler - Start");
+
+            _logger.LogInformation("generating specification from request");
+            var specification = new EventVenueSpecification(request);
+
+            _logger.LogInformation("retrieving result from manager");
+            var result = await _manager.GetAsync(specification);
+
+            _logger.LogInformation($"mapping result to dto");
+            var data = _mapper.Map<IEnumerable<EventVenueEntity>, IEnumerable<EventVenueListItemDto>>(result);
+
+            _logger.LogInformation($"creating {nameof(QueryResult)} from mapped data");
+            var queryResult = QueryResult<IEnumerable<EventVenueListItemDto>>.New(data);
+
+            _logger.LogInformation($"{nameof(GetVenuesHandler)} - Handler - End");
+
+            return queryResult;
+        }
     }
 }
---
+++ src/UrbanEngine.Core/Models/Venues/EventVenueDetailDto.cs
@@ -0,0 +1,23 @@
+﻿namespace UrbanEngine.Core.Models.Venues
+{
+    public class EventVenueDetailDto
+    {
+        public  long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Region { get; set; }
+    }
+}
