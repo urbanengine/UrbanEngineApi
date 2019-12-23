d4bb87b3331fb52e726f8b3eebd0aaac7332e6f0
scottluskcis <scottlusk.cis@gmail.com>
2019-12-23 11:20:26 -0500

    create new solution to address upgrade issues

---
+++ src/UrbanEngine.SharedKernel/Filters/IFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace UrbanEngine.SharedKernel.Filters
+{
+    /// <summary>
+    /// represents an object used to apply a filter
+    /// </summary>
+    public interface IFilter<T> where T : class
+    {
+        /// <summary>
+        /// returns an expression that can be used in Linq predicates
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        Expression<Func<T, bool>> GetExpression();
+    }
+}
