1b7588bcee04fe3837840a32ed517fdc1823b86c
scottluskcis <scottlusk.cis@gmail.com>
2019-12-23 13:52:41 -0500

    add swagger

--- src/UrbanEngine.Web/Startup.cs
+++ src/UrbanEngine.Web/Startup.cs
@@ -1,20 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using UrbanEngine.SharedKernel.Results;
 
 namespace UrbanEngine.Web
 {
     public class Startup
     {
+        static int _errorEventId = 1;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +35,32 @@ namespace UrbanEngine.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Urban Engine API",
+                    Version = "v1",
+                    Description = "Urban Engine API",
+                    // TermsOfService = new Uri(""),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Tyler Hughes",
+                        Email = "tyler@urbanengine.org"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Apache 2.0",
+                        Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +70,38 @@ namespace UrbanEngine.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    // get diagnostic information about the error 
+                    var exceptionHandlerPathFeature =
+                        context.Features.Get<IExceptionHandlerPathFeature>();
+
+                    // get the exception that was thrown from endpoint
+                    var exceptionThrown = exceptionHandlerPathFeature.Error;
+
+                    // return a status code and generic json message 
+                    context.Response.StatusCode = FailureResult.GetStatusCode(exceptionThrown);
+                    context.Response.ContentType = "application/json";
+
+                    var json = JsonConvert.SerializeObject(new FailureResult(exceptionThrown));
+                    await context.Response.WriteAsync(json);
+
+                    // log the error 
+                    var logger = errorApp.ApplicationServices.GetService<ILogger<Program>>();
+                    logger.LogError(_errorEventId++, exceptionThrown, $"exception caught in UseExceptionHandler middleware, see exception for details");
+                });
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Urban Engine API v1");
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
