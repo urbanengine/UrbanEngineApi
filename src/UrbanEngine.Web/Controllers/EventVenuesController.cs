c1453497257bc1193ee1dc070fbe3881946d4f37
scottluskcis <scottlusk.cis@gmail.com>
2019-12-25 20:53:21 -0500

    wire up controller endpoint for get

--- src/UrbanEngine.Web/Controllers/EventVenuesController.cs
+++ src/UrbanEngine.Web/Controllers/EventVenuesController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UrbanEngine.Core.Messages.Venues;
 
