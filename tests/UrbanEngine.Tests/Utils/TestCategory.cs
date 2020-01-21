bbf193099365a6cc6002e1e5d0d7cb5d7f16f6ec
scottluskcis <scottlusk.cis@outlook.com>
2020-01-21 13:31:43 -0500

    add util classes used

---
+++ tests/UrbanEngine.Tests/Utils/TestCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace UrbanEngine.Tests.Utils
+{
+    public class TestCategory
+    {
+        public const string Unit = "Unit";
+        public const string Integration = "Integration";
+        public const string Acceptance = "Acceptance";
+        public const string Load = "Load";
+    }
+}
