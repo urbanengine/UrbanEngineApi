9d35786da9cd671fd161decb45f5f613b2d5d126
scottluskcis <scottlusk.cis@outlook.com>
2020-02-27 23:10:31 -0500

    start woring on checking room availability

--- src/UrbanEngine.Core/Managers/Events/EventManager.cs
+++ src/UrbanEngine.Core/Managers/Events/EventManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using UrbanEngine.Core.Entities;
+using UrbanEngine.Core.Specifications.Events;
 using UrbanEngine.SharedKernel.Data;
 using UrbanEngine.SharedKernel.Managers;
 
@@ -11,14 +13,21 @@ namespace UrbanEngine.Core.Managers.Events
         public EventManager(IAsyncRepository<EventEntity> repository, ILogger<EventManager> logger) 
             : base(repository, logger) { }
 
-		public override Task<EventEntity> CreateAsync(EventEntity entity)
+		public async override Task<EventEntity> CreateAsync(EventEntity entity)
 		{
-			return base.CreateAsync(entity);
+			var roomIsAvailable = await IsRoomAvailableAsync(entity);
+			if(!roomIsAvailable)
+				throw new Exception(""); // TODO: create a custom exception and catch in the Handler
+
+			return await base.CreateAsync(entity);
 		}
 
-		public bool IsRoomAvailable()
-		{
-			return false;
+		public async Task<bool> IsRoomAvailableAsync(EventEntity entity)
+		{ 
+			var specification = new EventSpecification(null); // TODO: make a custom specification for filtering by room
+
+			var result = await Repository.AnyAsync(specification);
+			return result;
 		}
 	}
 }
