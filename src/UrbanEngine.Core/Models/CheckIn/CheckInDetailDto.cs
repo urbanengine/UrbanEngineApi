39e0f0b13a693a2abfc334ec246423f13d7a8dc1
scottluskcis <scottlusk.cis@outlook.com>
2020-10-18 17:16:52 -0400

    remove navigation property on dto, not needed

--- src/UrbanEngine.Core/Models/CheckIn/CheckInDetailDto.cs
+++ src/UrbanEngine.Core/Models/CheckIn/CheckInDetailDto.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using UrbanEngine.Core.Entities;
 
-namespace UrbanEngine.Core.Models.CheckIn {
-    public class CheckInDetailDto
+namespace UrbanEngine.Core.Models.CheckIn
+{
+	public class CheckInDetailDto
     {
-        #region Properties
         public long Id { get; set; }
         public bool IsDeleted { get; set; }
         public DateTimeOffset? DateCreated { get; set; }
         public DateTimeOffset CheckedInAt { get; set; }
         public long UserId { get; set; }
         public long? EventId { get; set; }
-
-        #endregion
-
-        #region Navigation Properties
-
-        public CheckInEntity Event { get; set; }
-
-        #endregion
     }
 }
